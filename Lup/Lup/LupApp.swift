a70c23b88cb26dcbbf8036605b02978f221ac69f
StanciuOctav <octav.stanciu00@gmail.com>
2025-06-27 14:03:34 +0300

    Created Xcode project

---
+++ Lup/Lup/LupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LupApp.swift
+//  Lup
+//
+//  Created by Octav Stanciu on 27.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct LupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

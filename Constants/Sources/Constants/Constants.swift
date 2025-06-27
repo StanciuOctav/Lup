24c5b977b38ac7a341ff1f37685990cd2b46ebf3
StanciuOctav <octav.stanciu00@gmail.com>
2025-06-27 15:09:54 +0300

    Updated Constants paths

--- Constants/Sources/Constants/Constants.swift
+++ Constants/Sources/Constants/Constants.swift
@@ -2,8 +2,8 @@
 // https://docs.swift.org/swift-book
 
 public struct Constants {
-    public static var mockURL: String { "http://demo\(Self.demoNumber).mockable.io/" }
+    public static var mockURL: String { "http://demo\(Self.demoNumber).mockable.io" }
     public static let demoNumber = 1051925
-    public static let getCustomersEndpoint: String = "customers"
-    public static let getOrdersEndpoint: String = "customers"
+    public static let getCustomersEndpoint: String = "/customers"
+    public static let getOrdersEndpoint: String = "/orders"
 }

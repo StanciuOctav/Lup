171c1b41c0e25f67a95795b7c0066a86150cfa1c
StanciuOctav <octav.stanciu00@gmail.com>
2025-06-29 15:24:08 +0300

    Changing the camera position when a route is active

--- Lup/Lup/TabViews/Customers/MapView.swift
+++ Lup/Lup/TabViews/Customers/MapView.swift
@@ -11,7 +11,6 @@ import SwiftUI
 
 struct MapView: View {
     @StateObject private var viewModel: MapViewModel
-    @State private var cameraPosition: MapCameraPosition = .automatic
     
     init(customersPublisher: AnyPublisher<[Customer], Never>, userLocationPublisher: AnyPublisher<CLLocation?, Never>) {
         self._viewModel = StateObject(wrappedValue: MapViewModel(customersPublisher: customersPublisher, userLocationPublisher: userLocationPublisher))
@@ -19,11 +18,13 @@ struct MapView: View {
     
     var body: some View {
         VStack {
-            Map(position: $cameraPosition) {
+            Map(position: $viewModel.cameraPosition) {
                 ForEach(viewModel.annotations) { annotation in
                     Annotation(annotation.customerName, coordinate: CLLocationCoordinate2D(latitude: annotation.latitude, longitude: annotation.longitude)) {
                         Image(systemName: "figure.stand")
+                            .frame(width: 50, height: 50)
                             .background(Circle().fill(.white).frame(width: 30, height: 30))
+                            .shadow(radius: 5)
                             .contextMenu {
                                 Button("Navigate to \(annotation.customerName)", systemImage: "arrow.turn.right.up") {
                                     viewModel.getDirections(to: annotation)
@@ -41,6 +42,15 @@ struct MapView: View {
             .mapControls {
                 MapUserLocationButton()
                 MapCompass()
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    viewModel.resetRoute()
+                } label: {
+                    Text("Reset")
+                }
             }
         }
     }
--- Lup/Lup/TabViews/Customers/MapViewModel.swift
+++ Lup/Lup/TabViews/Customers/MapViewModel.swift
@@ -18,11 +18,32 @@ struct CustomerAnnotation: Identifiable {
     var coordinate: CLLocationCoordinate2D { .init(latitude: latitude, longitude: longitude) }
 }
 
+fileprivate struct BoundingBox {
+    let minLat: Double
+    let maxLat: Double
+    let minLon: Double
+    let maxLon: Double
+    
+    var latPadding: Double {
+        (maxLat - minLat) * 0.3
+    }
+    
+    var lonPadding: Double {
+        (maxLon - minLon) * 0.3
+    }
+}
+
+fileprivate enum BoundingBoxOption {
+    case all, route
+}
+
 @MainActor
 final class MapViewModel: ObservableObject {
     @Published var userLocation: CLLocation?
     @Published var annotations: [CustomerAnnotation] = []
     @Published var route: MKRoute?
+    @Published var cameraPosition: MapCameraPosition = .automatic
+    
     private var customers: [Customer] = []
     private var cancellables: Set<AnyCancellable> = []
     
@@ -53,9 +74,58 @@ final class MapViewModel: ObservableObject {
             do {
                 let directions = try await MKDirections(request: request).calculate()
                 self?.route = directions.routes.first
+                
+                if let route = self?.route {
+                    self?.updateRouteCameraPosition(route: route, destination: newValue.coordinate)
+                }
             } catch {
                 print("Error updating route \(error)")
             }
+        }
+    }
+    
+    func resetRoute() {
+        route = nil
+        zoomToAllCustomers()
+    }
+    
+    private func zoomToAllCustomers() {
+        guard let box = boundingBox(for: .all) else { return }
+        updateCameraPosition(in: box)
+    }
+    
+    private func updateRouteCameraPosition(route: MKRoute, destination: CLLocationCoordinate2D) {
+        guard let box = boundingBox(for: .route, to: destination) else { return }
+        updateCameraPosition(in: box)
+    }
+    
+    private func updateCameraPosition(in box: BoundingBox) {
+        let center = CLLocationCoordinate2D(latitude: (box.minLat + box.maxLat) / 2, longitude: (box.minLon + box.maxLon) / 2)
+        let span = MKCoordinateSpan(latitudeDelta: (box.maxLat - box.minLat) + box.latPadding, longitudeDelta: (box.maxLon - box.minLon) + box.lonPadding)
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        withAnimation {
+            cameraPosition = .region(region)
+        }
+    }
+    
+    private func boundingBox(for option: BoundingBoxOption, to destination: CLLocationCoordinate2D? = nil) -> BoundingBox? {
+        switch(option) {
+        case .all:
+            guard !annotations.isEmpty else { return nil }
+            let latitudes = annotations.map { $0.latitude }
+            let longitudes = annotations.map { $0.longitude }
+            return BoundingBox(minLat: latitudes.min() ?? 0,
+                               maxLat: latitudes.max() ?? 0,
+                               minLon: longitudes.min() ?? 0,
+                               maxLon: longitudes.max() ?? 0)
+        case .route:
+            guard let userCoordinate = userLocation?.coordinate,
+            let destination else { return nil }
+            return BoundingBox(minLat: min(userCoordinate.latitude, destination.latitude),
+                               maxLat: max(userCoordinate.latitude, destination.latitude),
+                               minLon: min(userCoordinate.longitude, destination.longitude),
+                               maxLon: max(userCoordinate.longitude, destination.longitude))
         }
     }
     
